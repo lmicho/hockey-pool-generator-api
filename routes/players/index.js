20f632a925fd13dbac169c5d6015389d00518cc5
Lauriane Michaud <lauriane.michaud@nurun.com>
2019-01-20 15:27:59 -0500

    clean up comments

--- routes/players/index.js
+++ routes/players/index.js
@@ -107,51 +107,6 @@ router.route('/cumulative-player-stats/:playerId')
         });
     })
 
-
-
-
-
-/* 
-
-
-
-// daily games schedule by date
-router.route('/daily-game-schedule/:date')
-    .get(function (req, res) {
-        let date = req.params.date
-        request.get({ url: url + "/daily_game_schedule.json?fordate=" + date, headers: headers }, function (e, r, body) {
-            res.send(body)
-        });
-    })
-
-// game boxscore by game
-router.route('/game-boxscore/:gameId')
-    .get(function (req, res) {
-        let gameId = req.params.gameId;
-        request.get({ url: url + "/game_boxscore.json?gameid=" + gameId, headers: headers }, function (e, r, body) {
-            res.send(body)
-        });
-    })
-
-// game play by play by game
-router.route('/game-playbyplay/:gameId')
-    .get(function (req, res) {
-        let gameId = req.params.gameId;
-        request.get({ url: url + "/game_playbyplay.json?gameid=" + gameId, headers: headers }, function (e, r, body) {
-            res.send(body)
-        });
-    })
-
-// game starting line up by game
-router.route('/game-startinglineup/:gameId')
-    .get(function (req, res) {
-        let gameId = req.params.gameId;
-        request.get({ url: url + "/game_startinglineup.json?gameid=" + gameId, headers: headers }, function (e, r, body) {
-            res.send(body)
-        });
-    })
- */
-
 module.exports = router;
 
 
